170acd98f98b2eb1546be959f04da9a799f67aca
xiao <1102820661@qq.com>
2020-06-19 13:39:01 +0800

    fix 修改样式文件 为项目导入样式

--- pikachu/src/App.tsx
+++ pikachu/src/App.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+// import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Hello world</h1>
+        <h2>Hello world</h2>
+        <h3>Hello world</h3>
+        <hr/>
+        <code>
+          const a = b
+        </code>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
