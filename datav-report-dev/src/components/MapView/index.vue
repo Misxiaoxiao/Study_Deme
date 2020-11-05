4daac2d87fe87b47af5d8656741ab264264c61ff
chenxiaojun <1102820661@qq.com>
2020-11-05 10:32:55 +0800

    fix 初始化容器

---
+++ datav-report-dev/src/components/MapView/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    map view
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
--- datav-report-dev/src/views/Home.vue
+++ datav-report-dev/src/views/Home.vue
@@ -1,11 +1,35 @@
 <template>
   <div class="home">
-    report
+    <top-view />
+    <sales-view />
+    <bottom-view />
+    <map-view />
   </div>
 </template>
 
 <script>
+import TopView from '../components/TopView'
+import SalesView from '../components/SalesView'
+import BottomView from '../components/BottomView'
+import MapView from '../components/MapView'
+
 export default {
-  name: 'Home'
+  name: 'Home',
+  component: {
+    TopView,
+    SalesView,
+    BottomView,
+    MapView
+  }
 }
 </script>
+
+<style>
+  .home {
+    width: 100%;
+    height: 100%;
+    padding: 20px;
+    background: #eee;
+    box-sizing: border-box;
+  }
+</style>
