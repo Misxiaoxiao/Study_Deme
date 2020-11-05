0c355a531e7eab1608ae0bf8debd2071aa490365
chenxiaojun <1102820661@qq.com>
2020-11-05 10:20:05 +0800

    fix 添加 element 和 echarts

--- datav-report-dev/src/App.vue
+++ datav-report-dev/src/App.vue
@@ -1,10 +1,17 @@
 <template>
   <div id="app">
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
-<style lang="scss">
+<script>
+
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
 html, body, #app {
   width: 100%;
   height: 100%;
--- datav-report-dev/src/main.js
+++ datav-report-dev/src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import './plugins/element.js'
+import ECharts from 'echarts'
 
 Vue.config.productionTip = false
+Vue.prototype.$echarts = ECharts
 
 new Vue({
   router,
