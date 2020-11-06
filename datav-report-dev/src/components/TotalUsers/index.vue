c410b34583c03155fd5a79c08e69d1d3b19d1588
chenxiaojun <1102820661@qq.com>
2020-11-06 15:57:10 +0800

    fix 添加累计用户量组件

--- datav-report-dev/src/components/TotalUsers/index.vue
+++ datav-report-dev/src/components/TotalUsers/index.vue
@@ -1,6 +1,23 @@
 <template>
   <div>
-    <common-card />
+    <common-card
+      title="累计用户数"
+      value="1,087,503"
+    >
+      <template>
+        <div id="total-users-chart" :style="{ width: '100%', height: '100%' }" />
+      </template>
+      <template v-slot:footer>
+        <div class="total-users-footer">
+          <span>日同比</span>
+          <span class="emphasis">8.73%</span>
+          <span class="increase"></span>
+          <span class="month">月同比</span>
+          <span class="emphasis">35.91%</span>
+          <span class="decrease"></span>
+        </div>
+      </template>
+    </common-card>
   </div>
 </template>
 
@@ -8,9 +25,92 @@
 import commonCardMixin from '../../mixins/commonCardMixin'
 
 export default {
-  mixins: [commonCardMixin]
+  mixins: [commonCardMixin],
+  mounted () {
+    const chartDom = document.getElementById('total-users-chart')
+    const chant = this.$echarts.init(chartDom)
+    chant.setOption({
+      xAxis: {
+        type: 'value',
+        show: false
+      },
+      yAxis: {
+        type: 'category',
+        show: false
+      },
+      series: [{
+        type: 'bar',
+        stack: '总量',
+        data: [200],
+        barWidth: 10,
+        itemStyle: {
+          color: '#45c946'
+        }
+      }, {
+        type: 'bar',
+        stack: '总量',
+        data: [250],
+        barWidth: 10,
+        itemStyle: {
+          color: '#eee'
+        }
+      }, {
+        type: 'custom',
+        data: [200],
+        stack: '总量',
+        renderItem: (params, api) => {
+          const value = api.value(0)
+          const endPoint = api.coord([value, 0])
+          return {
+            type: 'group',
+            position: endPoint,
+            children: [{
+              type: 'path',
+              shape: {
+                d: 'M1024 255.996 511.971 767.909 0 255.996 1024 255.996z',
+                x: -5,
+                y: -20,
+                width: 10,
+                height: 10,
+                layout: 'cover'
+              },
+              style: {
+                fill: '#45c946'
+              }
+            }, {
+              type: 'path',
+              shape: {
+                d: 'M0 767.909l512.029-511.913L1024 767.909 0 767.909z',
+                x: -5,
+                y: 10,
+                width: 10,
+                height: 10,
+                layout: 'cover'
+              },
+              style: {
+                fill: '#45c946'
+              }
+            }]
+          }
+        }
+      }],
+      grid: {
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0
+      }
+    })
+  }
 }
 </script>
 
 <style lang="scss" scoped>
+.total-users-footer {
+  display: flex;
+  align-items: center;
+  .month {
+    margin-left: 10px;
+  }
+}
 </style>
