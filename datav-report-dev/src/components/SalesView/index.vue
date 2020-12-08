f995fa074b748394bd353e23e5c068392e8cd446
chenxiaojun <1102820661@qq.com>
2020-12-08 10:26:20 +0800

    fix 添加v-chart相关组件

--- datav-report-dev/src/components/SalesView/index.vue
+++ datav-report-dev/src/components/SalesView/index.vue
@@ -1,11 +1,27 @@
 <template>
   <div>
-    sales view
+    <v-chart
+      :options="data"
+    />
   </div>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      data: {
+        xAxis: {
+          type: 'category'
+        },
+        yAxis: {},
+        series: [{
+          type: 'line',
+          data: [100, 200, 300]
+        }]
+      }
+    }
+  }
 }
 </script>
 
--- datav-report-dev/src/main.js
+++ datav-report-dev/src/main.js
@@ -3,9 +3,11 @@ import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
 import ECharts from 'echarts'
+import VueECharts from 'vue-echarts'
 
 Vue.config.productionTip = false
 Vue.prototype.$echarts = ECharts
+Vue.component('v-chart', VueECharts)
 
 new Vue({
   router,
