d8cdd0e6f7cdf88ee4f90169e3dae85d459d9d08
chenxiaojun <1102820661@qq.com>
2020-12-21 17:53:37 +0800

    fix 添加飞线图测试文件

--- test-browser-node/config.js
+++ test-browser-node/config.js
@@ -1,5 +1,7 @@
+const baseUrl = 'http://223.72.254.131:14646/'
+
 module.exports = {
-  gotoUrl: 'http://223.72.254.131:14646/',
-  getCookieUrl: 'http://223.72.254.131:14646/task/getTaskByType',
-  serverUrl: 'http://223.72.254.131:14646'
+  gotoUrl: baseUrl,
+  getCookieUrl: baseUrl + 'task/getTaskByType',
+  serverUrl: baseUrl
 }
