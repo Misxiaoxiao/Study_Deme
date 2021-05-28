59983eec9b1a168450feb4576162f21c4f3d59f3
chenxiaojun <1102820661@qq.com>
2021-05-28 20:35:55 +0800

    style 修改列表页样式

--- jira/src/Authenticated-app.tsx
+++ jira/src/Authenticated-app.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import styled from '@emotion/styled'
 
 import { ProjectListScreen } from 'screens/project-list'
 import { useAuth } from 'context/authContext'
@@ -6,8 +7,40 @@ import { useAuth } from 'context/authContext'
 export const AuthenticatedApp: React.FC = () => {
   const { logout } = useAuth()
 
-  return <div>
-    <button onClick={() => { logout() } } >登出</button>
-    <ProjectListScreen />
-  </div>
+  return <Container>
+    <Header>
+      <HeaderLeft>
+        <h3>Logo</h3>
+        <h3>项目</h3>
+        <h3>用户</h3>
+      </HeaderLeft>
+      <HeaderRight>
+        <button onClick={logout}>登出</button>
+      </HeaderRight>
+    </Header>
+    <Main>
+      <ProjectListScreen />
+    </Main>
+  </Container>
 }
+
+const Container = styled.div`
+  display: grid;
+  grid-template-rows: 6rem 1fr;
+  height: 100vh;
+`;
+
+// grid-area 用来给grid子元素起名字
+const Header = styled.header`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+const HeaderLeft = styled.div`
+  display: flex;
+  align-items: center;
+`;
+const HeaderRight = styled.div``;
+const Main = styled.main`
+`;
