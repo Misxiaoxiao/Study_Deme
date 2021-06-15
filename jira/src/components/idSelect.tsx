cb0c41bc7ff7d8bf4c28e78800f8abecc8963918
chenxiaojun <1102820661@qq.com>
2021-06-15 09:39:05 +0800

    feat 添加 idSelect 组件

---
+++ jira/src/components/idSelect.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Select } from 'antd'
+import { RowType } from 'types'
+
+type SelectProps = React.ComponentProps<typeof Select>
+
+interface IdSelectPropsType extends Omit<SelectProps, 'value' | 'onChange' | 'options'> {
+  value: RowType | null | undefined;
+  onChange: (value?: number) => void;
+  defaultOptionName?: string;
+  options?: { name: string, id: number }[]
+}
+
+export const IdSelect: React.FC<IdSelectPropsType> = (props) => {
+  const { value, onChange, defaultOptionName, options, ...resetProps } = props
+  return <Select
+    value={toNumber(value)}
+    onChange={value => onChange(toNumber(value) || undefined)}
+  >
+    { defaultOptionName
+      ? <Select.Option value={0} {...resetProps}>{defaultOptionName}</Select.Option>
+      : null }
+    {
+      options?.map(
+        option => <Select.Option value={option.id}>{option.name}</Select.Option>
+      )
+    }
+  </Select>
+}
+
+const toNumber = (value: unknown) => isNaN(Number(value)) ? 0 : Number(value)
---
+++ jira/src/types/index.ts
@@ -0,0 +1,1 @@
+export type RowType = string | number;
