3d05c10d9cfb942279bb8e18c73a85cbd74b8b2d
chenxiaojun <1102820661@qq.com>
2021-06-17 16:17:55 +0800

    refactory 使用 useReducer 改造 DealCols

--- dealexcel/src/page/components/dealCols.tsx
+++ dealexcel/src/page/components/dealCols.tsx
@@ -1,7 +1,17 @@
-import React, { useState } from 'react'
+import React, { useReducer, useState } from 'react'
 import { Row, Button, Form, Input, Select } from 'antd'
 
-const initialRenderData = [
+type initialItemType = {
+  colName: string;
+  indexName: string;
+  searchVal: string;
+  isHas: boolean
+}
+
+type EditArgType = React.ChangeEvent<HTMLInputElement> | string | number | undefined;
+
+
+const initialRenderData: initialItemType[] = [
   {
     colName: '',
     indexName: '',
@@ -10,42 +20,91 @@ const initialRenderData = [
   }
 ]
 
-const DealCols: React.FC = () => {
-  const [renderData, setRenderData] = useState(initialRenderData)
+type ActionType = {
+  type: 'add' | 'delete' | 'edit';
+  data: EditArgType;
+  index?: number;
+  key?: keyof initialItemType;
+}
 
-  const addCol = (index: number) => {
-    const data = [...renderData]
-    data[index].isHas = true
-    data.push({
-      colName: '',
-      indexName: '',
-      searchVal: '',
-      isHas: false // 未添加
-    })
-    setRenderData(data)
-    // TODO 对应改变 context 中的 columns
+
+
+const renderDataReducer = (state: initialItemType[], action: ActionType) => {
+  const { type, data, index, key } = action
+
+  switch (type) {
+    case 'add': {
+      const curState = [...state]
+      const index = data as number
+      curState[index].isHas = true
+      curState.push({
+        colName: '',
+        indexName: '',
+        searchVal: '',
+        isHas: false // 未添加
+      })
+      return curState
+    }
+    case 'delete': {
+      const curState = [...state]
+      const index = data as number
+      curState.splice(index, 1)
+      return curState
+    }
+    case 'edit': {
+      const curState = [...state]
+      if (index === undefined || key === undefined) return state
+      if (typeof data === 'object') {
+        curState[index] = {
+          ...curState[index],
+          [key]: data.target.value
+        }
+      } else {
+        curState[index] = {
+          ...curState[index],
+          [key]: data
+        }
+      }
+      
+      return curState
+    }
+    default:
+      return state
   }
+}
 
-  const delCol = (index: number) => {}
+
+const DealCols: React.FC = () => {
+  const [renderData, dispatch] = useReducer(renderDataReducer, initialRenderData)
+
+  // TODO 对应改变 context 中的 columns
+  const addCol = (index: number) => dispatch({type: 'add', data: index})
+
+  const delCol = (index: number) => dispatch({type: 'delete', data: index})
+
+  const editCol = (index: number, key: keyof initialItemType) =>
+    (e: EditArgType) => dispatch({index, key, data: e, type: 'edit'})
 
   return <Row>
     { renderData.map((renderItem, index) => <Row style={{ marginBottom: '10px' }} key={index}>
       <Form.Item label="列名" style={{ marginLeft: '10px' }}>
         <Input
           value={renderItem.colName}
-          onChange={e => {}}
+          onChange={editCol(index, 'colName')}
         />
       </Form.Item>
       <Form.Item label="对应导入列" style={{ marginLeft: '10px' }}>
         <Select
+          options={[ { label: '1', value: 1 } ]}
           style={{ width: '300px' }}
           value={renderItem.indexName}
+          onChange={editCol(index, 'indexName')}
         />
       </Form.Item>
       <Form.Item label="搜索值" style={{ marginLeft: '10px' }}>
         <Input
           value={renderItem.searchVal}
-          onChange={e => {}}
+          onChange={editCol(index, 'searchVal')}
         />
       </Form.Item>
       { renderItem.isHas
