6f5e0e8567e2d3745a783060cf8d8bd8dce5f489
chenxiaojun <1102820661@qq.com>
2021-06-29 10:28:34 +0800

    feat init vue-music project

---
+++ vue-music/.eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'plugin:vue/essential',
+    'airbnb-base'
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module'
+  },
+  plugins: [
+    'vue',
+    '@typescript-eslint'
+  ],
+  rules: {
+    semi: [2, 'never'],
+    'comma-dangle': ['error', 'never'],
+    'vue/no-multiple-template-root': 'off'
+  }
+}
