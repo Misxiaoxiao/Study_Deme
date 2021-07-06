7789bc1050087cdac4158fb939eff380cbbe86a6
chenxiaojun <1102820661@qq.com>
2021-07-06 10:47:22 +0800

    feat add columnlist component

--- zheye/src/App.vue
+++ zheye/src/App.vue
@@ -1,27 +1,40 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="container">
+    <column-list :list="list" />
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import { defineComponent } from 'vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import ColumnList, { ColumnProps } from './components/ColumnList.vue'
+
+const testData: ColumnProps[] = [
+  {
+    id: 1,
+    title: 'test1的专栏',
+    description: '这是test1d的专栏，有一段非常有意思的简介，可以更新一下',
+    avatar: 'http://baidu.com',
+  },
+  {
+    id: 1,
+    title: 'test1的专栏',
+    description: '这是test1d的专栏，有一段非常有意思的简介，可以更新一下',
+    avatar: 'http://baidu.com',
+  },
+]
 
 export default defineComponent({
-  name: 'App',
   components: {
-    HelloWorld,
+    ColumnList,
   },
-});
+  setup() {
+    return {
+      list: testData,
+    }
+  },
+})
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
 </style>
