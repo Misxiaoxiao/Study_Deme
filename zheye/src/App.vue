1f5d2e1976d49633ea59d6c89d79ac046d7b4b09
chenxiaojun <1102820661@qq.com>
2021-07-06 15:42:25 +0800

    feat add ValidateInput

--- zheye/src/App.vue
+++ zheye/src/App.vue
@@ -3,6 +3,10 @@
     <global-header :user="currentUser" />
     <form action="">
       <div class="mb-3">
+        <label class="form-label">邮箱地址</label>
+        <validate-input :rules="emailRules" />
+      </div>
+      <div class="mb-3">
         <label for="exambleInputEmail1" class="form-label">邮箱地址</label>
         <input
           type="email" class="form-control" id="exampleInputEmail1"
@@ -23,33 +27,26 @@
 import { defineComponent, reactive } from 'vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import GlobalHeader, { UserProps } from './components/GlobelHeader.vue'
+import ValidateInput, { RulesProp } from './components/ValidateInput.vue'
 
 const currentUser: UserProps = {
   isLogin: true,
   name: 'xiao',
 }
 
-// const testData: ColumnProps[] = [
-//   {
-//     id: 1,
-//     title: 'test1的专栏',
-//     description: '这是test1d的专栏，有一段非常有意思的简介，可以更新一下，可以更新一下可以更新一下可以更新一下可以更新一下可以更新一下可以更新一下可以更新一下',
-//     avatar: 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fb1-q.mafengwo.net%2Fs7%2FM00%2F2E%2F37%2FwKgB6lSuHraAYMNtAAXF8ovILuk683.png&refer=http%3A%2F%2Fb1-q.mafengwo.net&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1628132261&t=9ddee0c2aa9b47a632fb34a3d61715ae',
-//   },
-//   {
-//     id: 1,
-//     title: 'test1的专栏',
-//     description: '这是test1d的专栏，有一段非常有意思的简介，可以更新一下',
-//   },
-// ]
-
 const emailReg = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
 
 export default defineComponent({
   components: {
     GlobalHeader,
+    ValidateInput,
   },
   setup() {
+    const emailRules: RulesProp = [
+      { type: 'required', message: '电子邮箱地址不能为空' },
+      { type: 'email', message: '请输入正确的电子邮箱格式' },
+    ]
+
     const emailRef = reactive({
       val: '',
       error: false,
@@ -71,6 +68,7 @@ export default defineComponent({
       currentUser,
       emailRef,
       validateEmail,
+      emailRules,
     }
   },
 })
