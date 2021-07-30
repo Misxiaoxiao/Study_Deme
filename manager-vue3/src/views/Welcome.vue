743e078b832f17171f44d2561af141b87db65ecb
chenxiaojun <1102820661@qq.com>
2021-07-30 13:48:20 +0800

    feat 添加首页样式的开发

--- manager-vue3/src/views/Welcome.vue
+++ manager-vue3/src/views/Welcome.vue
@@ -1,8 +1,11 @@
 <template>
   <div class="welcome">
-    <h1>welcome page title</h1>
-    this is welcome page
-    <router-link to="login">go to login</router-link>
+    <div class="content">
+      <div class="sub-title">欢迎体验</div>
+      <div class="tilte">后台管理系统</div>
+      <div class="desc">描述信息</div>
+    </div>
+    <div class="img" />
   </div>
 </template>
 
@@ -18,5 +21,37 @@ export default defineComponent({
 })
 </script>
 
-<style scoped>
+<style lang="scss">
+.welcome {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  background-color: #fff;
+  .content {
+    position: relative;
+    bottom: 40px;
+    .sub-title {
+      font-size: 30px;
+      line-height: 42px;
+      color: #333;
+    }
+    .title {
+      font-size: 40px;
+      line-height: 62px;
+      color: #409eff;
+    }
+    .desc {
+      text-align: right;
+      font-size: 14px;
+      color: #999;
+    }
+  }
+  .img {
+    margin-left: 105px;
+    background-image: url("./../assets/images/welcome.png");
+    width: 371px;
+    height: 438px;
+  }
+}
 </style>
