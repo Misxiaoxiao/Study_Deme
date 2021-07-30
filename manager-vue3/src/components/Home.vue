7b2df96be8d7cbb43bfa28a99132111143d6b7a5
chenxiaojun <1102820661@qq.com>
2021-07-30 14:25:34 +0800

    feat 添加菜单

--- manager-vue3/src/components/Home.vue
+++ manager-vue3/src/components/Home.vue
@@ -1,9 +1,46 @@
 <template>
   <div class="basic-layout">
-    <div class="nav-side"></div>
+    <div class="nav-side">
+      <!-- 系统logo -->
+      <div class="logo">
+        <img src="../assets/logo.png" />
+        <span>Manager</span>
+      </div>
+      <!-- 导航菜单 -->
+      <el-menu
+        default-active="2"
+        class="nav-menu"
+        background-color="#001529"
+        text-color="#fff"
+        router
+        :collapse="false"
+      >
+        <el-submenu index="1">
+          <template #title>
+            <i class="el-icon-setting"></i>
+            <span>系统管理</span>
+          </template>
+          <el-menu-item index="1-1">用户管理</el-menu-item>
+          <el-menu-item index="1-2">菜单管理</el-menu-item>
+        </el-submenu>
+        <el-submenu index="2">
+          <template #title>
+            <i class="el-icon-setting"></i>
+            <span>审批管理</span>
+          </template>
+          <el-menu-item index="2-1">休假申请</el-menu-item>
+          <el-menu-item index="2-2">待我审批</el-menu-item>
+        </el-submenu>
+      </el-menu>
+    </div>
     <div class="content-right">
       <div class="nav-top">
-        <div class="bread">面包屑</div>
+        <div class="nav-left">
+          <div class="menu-fold">
+            <i class="el-icon-s-fold" />
+          </div>
+          <div class="bread">面包屑</div>
+        </div>
         <div class="user">用户</div>
       </div>
       <div class="wrapper">
@@ -16,12 +53,12 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent } from "vue"
 
 export default defineComponent({
-  setup () {
+  setup() {
     return {}
-  }
+  },
 })
 </script>
 
@@ -35,10 +72,41 @@ export default defineComponent({
     background-color: #001529;
     color: #fff;
     overflow-y: auto;
-    transition: width .5s;
+    transition: width 0.5s;
+    .logo {
+      display: flex;
+      align-items: center;
+      font-size: 18px;
+      height: 50px;
+      img {
+        margin: 0 16px;
+        width: 32px;
+        height: 32px;
+      }
+    }
+    .nav-menu {
+      height: calc(100vh - 50px);
+      border-right: none;
+    }
+    // 合并
+    &.fold {
+      width: 64px;
+    }
+    // 展开
+    &.unfold {
+      width: 200px;
+    }
   }
   .content-right {
     margin-left: 200px;
+    // 合并
+    &.fold {
+      margin-left: 64px;
+    }
+    // 展开
+    &.unfold {
+      margin-left: 200px;
+    }
     .nav-top {
       height: 50px;
       line-height: 50px;
@@ -46,6 +114,25 @@ export default defineComponent({
       justify-content: space-between;
       border-bottom: 1px solid #ddd;
       padding: 0 20px;
+      .nav-left {
+        display: flex;
+        align-items: center;
+        .menu-fold {
+          margin-right: 15px;
+          font-size: 18px;
+        }
+      }
+      .user-info {
+        .notice {
+          line-height: 30px;
+          margin-right: 15px;
+          cursor: pointer;
+        }
+        .user-link {
+          cursor: pointer;
+          color: #409eff;
+        }
+      }
     }
     .wrapper {
       background: #eef0f3;
@@ -55,7 +142,6 @@ export default defineComponent({
         background: #fff;
         height: 100%;
       }
-      // background-color: #ffffff;
     }
   }
 }
